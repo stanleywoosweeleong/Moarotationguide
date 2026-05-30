bf85d9d616918d7636e3676f0b2487744739d763
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-05-30 09:49:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  *
  * Bump CACHE_VERSION if you ever need to force every device to drop old caches.
  */
-const CACHE_VERSION = 'pestmoa-v4';
+const CACHE_VERSION = 'pestmoa-v5';
 
 self.addEventListener('install', (event) => {
   // Activate this worker as soon as it finishes installing.
